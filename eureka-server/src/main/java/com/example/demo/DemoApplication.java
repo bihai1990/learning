3726e84b280445cd7443bdd62f7442cbc16c2974
bihai1990 <184619873@qq.com>
2019-05-29 00:12:31 +0800

    test

--- eureka-server/src/main/java/com/example/demo/DemoApplication.java
+++ eureka-server/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("-------------");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
