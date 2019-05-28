fd3bd8332f039302dac0717955b01d8212d67ca4
bihai1990 <184619873@qq.com>
2019-05-29 00:14:41 +0800

    删除打印语句

--- eureka-server/src/main/java/com/example/demo/DemoApplication.java
+++ eureka-server/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("-------------");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
