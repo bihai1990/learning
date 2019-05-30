f9b7bd4ea742e0e0840fe0b4539f002b2eb0efab
bihai1990 <184619873@qq.com>
2019-05-30 22:17:32 +0800

    1.配置eureka服务端及客户端 2.关闭eureka服务端的自我保护机制，确保注册中心能够剔除不可用的实例 3.注意server端和client端eureka.client.serviceUrl.defaultZone的配置

--- eureka-client/src/main/java/com/example/demo/ClientApplication.java
+++ eureka-client/src/main/java/com/example/demo/ClientApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableEurekaClient
+public class ClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ClientApplication.class, args);
 	}
 
 }
