92dbc4caed9d66a87cf9fa8de89cb779fc2e2fc1
bihai1990 <184619873@qq.com>
2019-06-20 21:53:47 +0800

    1.配置Feign工程

---
+++ eureka-feign/src/main/java/com/example/demo/controller/FeignController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.FeignService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FeignController {
+
+    @Autowired
+    private FeignService feignService;
+
+    @RequestMapping(value = "/feign-hi", method = RequestMethod.GET)
+    public String hi(@RequestParam(value = "name", defaultValue = "feign-test") String name) {
+        return feignService.feignMethod(name);
+    }
+}
---
+++ eureka-feign/src/main/java/com/example/demo/service/FeignService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "eureka-client")
+public interface FeignService {
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String feignMethod(@RequestParam(value = "feign") String name);
+}
